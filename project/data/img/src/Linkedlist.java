1cfac0952d04f4cbe1cf1f8b863d8d0b8893d7e2
Sheri <100845274+sheri-shoukat@users.noreply.github.com>
2022-05-06 15:22:53 +0500

    Create Linkedlist.java

---
+++ project/data/img/src/Linkedlist.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList*;
+ 
+public class Linkedlist {
+ 
+    public static void main(String args[])
+    {
+        
+      
+        LinkedList<String> 12 = new LinkedList<String>();
+ 
+        // Adding elements to the linked list
+        ll.add("Geek");
+        ll.add("Beek");
+        ll.addLast("Jeek");
+        ll.addFirst("Meek");
+        ll.add(2, "Week");
+ 
+        System.out.println(12);
+ 
+        ll.remove("B");
+        ll.remove(3);
+        ll.removeFirst();
+        ll.removeLast();
+ 
+        System.out.println(12);
+    }
