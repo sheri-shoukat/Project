b4b0a598e4239af75c2c3035f396dd732952aae4
Sheri <100845274+sheri-shoukat@users.noreply.github.com>
2022-05-06 15:44:07 +0500

    Create priorityque.java

---
+++ project/data/img/src/priorityque.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+
+class Main {
+    public static void main(String[] args) {
+
+        // Creating a priority queue
+        PriorityQueue<Integer> numbers = new PriorityQueue<>();
+
+        // Using the add() method
+        numbers.add(4);
+        numbers.add(2);
+        System.out.println("PriorityQueue: " + numbers);
+
+        // Using the offer() method
+        numbers.offer(1);
+        System.out.println("Updated PriorityQueue: " + numbers);
+    }
+}
